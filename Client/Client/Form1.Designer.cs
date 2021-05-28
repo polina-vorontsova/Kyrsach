a93754251426b6a62c7414b3e98acf34bd331233
polina-vorontsova <71218055+polina-vorontsova@users.noreply.github.com>
2021-05-28 18:30:21 +0300

    Add files via upload

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Client
+{
+    partial class formClient
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formClient));
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.rdpViewer = new AxRDPCOMAPILib.AxRDPViewer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(468, 3);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 67);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDisconnect.Location = new System.Drawing.Point(594, 3);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(120, 67);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.ColumnCount = 5;
+            this.MainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.MainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.MainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.MainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.MainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.MainPanel.Controls.Add(this.rdpViewer, 0, 1);
+            this.MainPanel.Controls.Add(this.label1, 0, 0);
+            this.MainPanel.Controls.Add(this.txtIP, 1, 0);
+            this.MainPanel.Controls.Add(this.btnConnect, 2, 0);
+            this.MainPanel.Controls.Add(this.btnDisconnect, 3, 0);
+            this.MainPanel.Controls.Add(this.btnSend, 4, 0);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.RowCount = 2;
+            this.MainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.MainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.MainPanel.Size = new System.Drawing.Size(846, 491);
+            this.MainPanel.TabIndex = 4;
+            // 
+            // rdpViewer
+            // 
+            this.rdpViewer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MainPanel.SetColumnSpan(this.rdpViewer, 5);
+            this.rdpViewer.Enabled = true;
+            this.rdpViewer.Location = new System.Drawing.Point(3, 76);
+            this.rdpViewer.Name = "rdpViewer";
+            this.rdpViewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdpViewer.OcxState")));
+            this.rdpViewer.Size = new System.Drawing.Size(840, 412);
+            this.rdpViewer.TabIndex = 5;
+            this.rdpViewer.OnConnectionEstablished += new System.EventHandler(this.rdpViewer_OnConnectionEstablished);
+            this.rdpViewer.OnConnectionFailed += new System.EventHandler(this.rdpViewer_OnConnectionFailed);
+            this.rdpViewer.OnConnectionTerminated += new AxRDPCOMAPILib._IRDPSessionEvents_OnConnectionTerminatedEventHandler(this.rdpViewer_OnConnectionTerminated);
+            this.rdpViewer.OnError += new AxRDPCOMAPILib._IRDPSessionEvents_OnErrorEventHandler(this.rdpViewer_OnError);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter IP:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIP.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtIP.Location = new System.Drawing.Point(172, 20);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(290, 32);
+            this.txtIP.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(720, 3);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(123, 67);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send File";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // formClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 491);
+            this.Controls.Add(this.MainPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(700, 350);
+            this.Name = "formClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formClient_FormClosing);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpViewer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TableLayoutPanel MainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private AxRDPCOMAPILib.AxRDPViewer rdpViewer;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using AxRDPCOMAPILib;
+using System.Threading;
+
+namespace Client
+{
+    public partial class formClient : Form
+    {
+        private const int BUF_SIZE = 16384;
+        private const int PORT = 5000;
+        private const string USER_NAME = "POLINA";
+        private const string PASSWORD = "";
+        private Socket sockServer = null;
+        private Thread WaitCommand;
+
+        private void StartWaitingCommand()
+        {
+            WaitCommand = new Thread(Wait);
+            WaitCommand.IsBackground = true;
+            WaitCommand.Start();
+        }
+
+        private void Wait()
+        {
+            while (sockServer != null)
+            {
+                byte[] buffer = new byte[BUF_SIZE];
+                int recbytes = sockServer.Receive(buffer);
+                ParseCommand(Encoding.Unicode.GetString(buffer, 0, recbytes));
+            }
+        }
+
+        private void ParseCommand(string Command)
+        {
+            string[] args = Command.Split('|');
+            string command = args[0];
+            switch (command)
+            {
+                case "fileYes":
+                    break;
+            }
+
+        }
+
+        public formClient()
+        {
+            InitializeComponent();
+            rdpViewer.SmartSizing = true;
+            this.BackColor = Color.FromArgb(204, 255, 204);
+            foreach (var control in MainPanel.Controls)
+            {
+                Button but = control as Button;
+                if (but != null )
+                    but.BackColor = Color.FromArgb(255, 255, 255);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse(txtIP.Text);
+                sockServer = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sockServer.Connect(ip, PORT);
+
+                byte[] buffer = new byte[BUF_SIZE];
+                int recbytes = sockServer.Receive(buffer);
+                string invitation = Encoding.Unicode.GetString(buffer, 0, recbytes);
+
+                RDPConnect(invitation, this.rdpViewer, USER_NAME, PASSWORD);
+
+                StartWaitingCommand();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unable to connect!");
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            Disconnect(rdpViewer);
+        }
+
+        private void RDPConnect(string invitation, AxRDPViewer display, string UserName, string Password)
+        {
+            try
+            {
+                display.Connect(invitation, UserName, Password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to connect. \nException :  {ex.Message}");
+            }
+        }
+
+        private void Disconnect(AxRDPViewer display)
+        {
+            display.Disconnect();
+        }
+
+
+        private void formClient_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            rdpViewer.Disconnect();
+        }
+
+        private void rdpViewer_OnConnectionEstablished(object sender, EventArgs e)
+        {
+            btnSend.Enabled = true;
+            txtIP.Enabled = false;
+            btnConnect.Enabled = false;
+            MessageBox.Show("Connection Established!");
+        }
+
+        private void rdpViewer_OnConnectionFailed(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = true;
+            txtIP.Enabled = true;
+            btnSend.Enabled = false;
+            MessageBox.Show($"ConnectionError");
+        }
+
+        private void rdpViewer_OnConnectionTerminated(object sender, _IRDPSessionEvents_OnConnectionTerminatedEvent e)
+        {
+            btnConnect.Enabled = true;
+            txtIP.Enabled = true;
+            btnSend.Enabled = false;
+            sockServer = null;
+            MessageBox.Show($"Connection Terminated. Reason: {e.discReason}");
+        }
+
+        private void rdpViewer_OnError(object sender, _IRDPSessionEvents_OnErrorEvent e)
+        {
+            int errorCode = (int)e.errorInfo;
+            MessageBox.Show($"Error 0x{errorCode}");
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (sockServer != null)
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        byte[] buffer = File.ReadAllBytes(dialog.FileName);
+                        FileInfo info = new FileInfo(dialog.FileName);
+                        sockServer.Send(Encoding.Unicode.GetBytes($"file|{info.FullName}|{buffer.Length}|"));
+                        sockServer.Send(buffer);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show($"Exception : {ex.Message}");
+                    }
+                }
+            }
+        }
+
+    }
+}
